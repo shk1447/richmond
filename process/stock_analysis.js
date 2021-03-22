77253389ad069590edc63b04830074230cc354f3
seokhwankim <seokhwan.kim@makinarocks.ai>
2021-03-22 17:32:11 +0900

    modify analysis

--- process/stock_analysis.js
+++ process/stock_analysis.js
@@ -31,6 +31,9 @@ var d = JSON.parse(fs.readFileSync(path.resolve(__dirname, './data/' + cmd.code 
 var trend_result = {
   date: '',
   init_trend: null,
+  close:0,
+  high:0,
+  low:0,
   up_volume_per: 0,
   down_volume_per: 0,
   up_avg_degree: 0,
@@ -39,7 +42,9 @@ var trend_result = {
   downward_type: [],
   type: [],
   upward: [],
-  downward: []
+  downward: [],
+  after_upwards:[],
+  after_downwards:[]
 }
 
 var up_cnt = 0;
@@ -61,6 +66,7 @@ for (var i = days + 7; i >= days; i--) {
   var result = {
     name: cmd.stock_name,
     total: cmd.stock_total,
+    close: 0,
     curr_trend: null,
     prev_point: [],
     curr_point: [],
@@ -73,6 +79,9 @@ for (var i = days + 7; i >= days; i--) {
   var valid_arr = [...d.filter(function (d) { return d.date <= (test.unix() * 1000) })];
   segmentation(valid_arr, result);
   prev_price = valid_arr[valid_arr.length - 1].close
+  trend_result.close = prev_price;
+  trend_result.high = valid_arr[valid_arr.length - 1].high;
+  trend_result.low = valid_arr[valid_arr.length - 1].low;
 
   result.curr_point.forEach(function (d) {
     if (result.curr_trend == 'downward') {
@@ -148,22 +157,60 @@ trend_result.down_volume_per = Math.floor(down_avg_volume / parseFloat(cmd.stock
 trend_result.up_avg_degree = up_avg_degree;
 trend_result.down_avg_degree = down_avg_degree;
 
-if (up_avg_volume > down_avg_volume) {
-  if (trend_result.upward.length >= trend_result.downward.length) {
-    if (trend_result.upward[trend_result.upward.length - 1].degree > trend_result.downward[trend_result.downward.length - 1].degree) {
-      if (trend_result.upward[trend_result.upward.length - 1].avg_volume > trend_result.downward[trend_result.downward.length - 1].avg_volume) {
-        if (down_avg_degree > trend_result.downward[trend_result.downward.length - 1].degree && up_avg_degree <= trend_result.upward[trend_result.upward.length - 1].degree) {
+if(trend_result.init_trend == 'upward') {
+  if(trend_result.upward.length > 0) {
+    var last_upward = trend_result.upward[trend_result.upward.length - 1];
+    var after_downwards = trend_result.downward.filter(function(d) { return moment(d.date) >= moment(last_upward.date)});
+    if(after_downwards.length > 0) {
+      trend_result.after_downwards = after_downwards;
+      if(trend_result.upward.length < trend_result.downward.length && trend_result.upward.length > trend_result.after_downwards.length) {
+        if(last_upward.low <= trend_result.low) {
           if (curr_price > prev_price) {
             fsPath.writeFileSync(path.resolve(__dirname, './analysis_success/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
           } else {
             fsPath.writeFileSync(path.resolve(__dirname, './analysis_fail/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
           }
         }
-        // fsPath.writeFileSync(path.resolve(__dirname, './analysis/' + trend_result.init_trend + "/" + trend_result.up_volume_per + "_" +cmd.stock_name+'_' +cmd.code +'.json'), JSON.stringify(trend_result,null,2))
+      }
+    }
+  }
+} else {
+  if(trend_result.downward.length > 0) {
+    var last_downward = trend_result.downward[trend_result.downward.length - 1];
+    var after_upwards = trend_result.upward.filter(function(d) { return moment(d.date) >= moment(last_downward.date)});
+    if(after_upwards.length > 0) {
+      trend_result.after_upwards = after_upwards;
+      if(trend_result.upward.length > trend_result.downward.length && trend_result.downward.length > trend_result.after_upwards.length) {
+        if(trend_result.high >= last_downward.high) {
+          if (curr_price > prev_price) {
+            fsPath.writeFileSync(path.resolve(__dirname, './analysis_success/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
+          } else {
+            fsPath.writeFileSync(path.resolve(__dirname, './analysis_fail/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
+          }
+        }
       }
     }
   }
 }
+console.log(trend_result)
+
+
+// if (up_avg_volume > down_avg_volume) {
+//   if (trend_result.upward.length >= trend_result.downward.length) {
+//     if (trend_result.upward[trend_result.upward.length - 1].degree > trend_result.downward[trend_result.downward.length - 1].degree) {
+//       if (trend_result.upward[trend_result.upward.length - 1].avg_volume > trend_result.downward[trend_result.downward.length - 1].avg_volume) {
+//         if (down_avg_degree > trend_result.downward[trend_result.downward.length - 1].degree && up_avg_degree <= trend_result.upward[trend_result.upward.length - 1].degree) {
+//           if (curr_price > prev_price) {
+//             fsPath.writeFileSync(path.resolve(__dirname, './analysis_success/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
+//           } else {
+//             fsPath.writeFileSync(path.resolve(__dirname, './analysis_fail/' + trend_result.init_trend + "/" + Math.floor(curr_price / prev_price * 100) + "_" + trend_result.up_volume_per + "_" + cmd.stock_name + '_' + cmd.code + '.json'), JSON.stringify(trend_result, null, 2))
+//           }
+//         }
+//         // fsPath.writeFileSync(path.resolve(__dirname, './analysis/' + trend_result.init_trend + "/" + trend_result.up_volume_per + "_" +cmd.stock_name+'_' +cmd.code +'.json'), JSON.stringify(trend_result,null,2))
+//       }
+//     }
+//   }
+// }
 
 // console.log(trend_result)
 
@@ -217,7 +264,7 @@ var t1 = performance.now();
 console.log(t1 - t0);
 
 
-function trend_analysis(data, result, trend_type) {
+function trend_analysis(data, result, trend_type, firstValue) {
   if (data.length > 1) {
     var start_x = data[0].date / 1000000;
     var start_y = data[0].close;
@@ -227,8 +274,12 @@ function trend_analysis(data, result, trend_type) {
 
     var min_idx = null;
     var min_degree = 0;
+    var diff = 0;
     var _cnt = 0;
     var _volume = 0;
+    var _close = 0;
+    var _high = 0;
+    var _low = 0;
     data.forEach((d, i) => {
       var dynamic_x = d.date / 1000000;
       var dynamic_y = d.close;
@@ -238,19 +289,27 @@ function trend_analysis(data, result, trend_type) {
       if (std_degree > dynamic_degree && dynamic_degree != 0) {
         if (min_degree > 0) {
           if (dynamic_degree < min_degree) {
-            min_degree = dynamic_degree
+            _high = d.high;
+            _low = d.low;
+            _close = dynamic_y;
+            min_degree = dynamic_degree;
+            diff = (dynamic_y - firstValue) / _cnt
             min_idx = i;
           }
         } else {
+          _high = d.high;
+          _low = d.low;
+          _close = dynamic_y;
           min_degree = dynamic_degree
+          diff = (dynamic_y - firstValue) / _cnt
           min_idx = i;
         }
       }
     })
 
     if (min_idx) {
-      result.curr_point.push({ avg_volume: _volume / _cnt, degree: min_degree, date: moment(data[min_idx].date).format('YYYY-MM-DD') })
-      trend_analysis(data.slice(min_idx, data.length), result, trend_type)
+      result.curr_point.push({ avg_volume: _volume / _cnt, degree: min_degree, high:_high, low:_low, close:_close, diff:diff, date: moment(data[min_idx].date).format('YYYY-MM-DD') })
+      trend_analysis(data.slice(min_idx, data.length), result, trend_type, data[min_idx].close)
     }
   }
 }
@@ -275,7 +334,7 @@ function segmentation(data, result) {
           result.prev_point = [...result.curr_point]
           result.curr_point = [];
         }
-        trend_analysis(data.slice(min_idx, max_idx + 1), result, trend_type);
+        trend_analysis(data.slice(min_idx, max_idx + 1), result, trend_type, min.close);
         data.splice(0, max_idx);
         result.curr_trend = trend_type
         segmentation(data, result);
@@ -285,7 +344,7 @@ function segmentation(data, result) {
           result.prev_point = [...result.curr_point]
           result.curr_point = [];
         }
-        trend_analysis(data.slice(max_idx, min_idx + 1), result, trend_type);
+        trend_analysis(data.slice(max_idx, min_idx + 1), result, trend_type, max.close);
         data.splice(0, min_idx);
         result.curr_trend = trend_type;
         segmentation(data, result)
@@ -293,4 +352,3 @@ function segmentation(data, result) {
     }
   }
 }
-
